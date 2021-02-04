412f6cd228ac32703d3053478e7fb3f3afba4f0e
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-02-03 21:31:57 -0700

    playing around!

--- problems.js
+++ problems.js
@@ -2,6 +2,6 @@
 // [23, 140, 212, 41] => [-5, 60, 100, 5]
 // (32°F − 32) × 5/9 = 0°C
 
-function getCelcius(farenheit) {
-  return farenheit.map(value => (value -32) * 5/9)
+function tempChange(farenheit) {
+  return farenheit.map(value => (value - 32) * 5 / 9)
 }
